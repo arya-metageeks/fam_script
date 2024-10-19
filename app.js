d8bc3b638f6bdcaf7e0e5ccd002fc6fa63b60b5f
kanhaiya83 <kanhaiya2778@gmail.com>
2024-10-19 13:13:56 +0000

    build

--- app.js
+++ app.js
@@ -18,16 +18,19 @@ const fs_1 = __importDefault(require("fs"));
 const express_1 = __importDefault(require("express"));
 const date_fns_1 = require("date-fns");
 const mongoose_1 = __importDefault(require("mongoose"));
-const body_parser_1 = __importDefault(require("body-parser"));
 const unique_names_generator_1 = require("unique-names-generator");
+const multer_1 = __importDefault(require("multer"));
+const csv_parse_1 = require("csv-parse");
+const axios_1 = __importDefault(require("axios"));
 const config = {
     dictionaries: [unique_names_generator_1.names]
 };
 dotenv_1.default.config();
+const BUFFERED_ETH_MULTIPLIER = Number(process.env.BUFFERED_ETH_MULTIPLIER) || 10;
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
-app.use(body_parser_1.default.json());
-app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
 mongoose_1.default.connect(process.env.MONGODB_URI, { dbName: process.env.DB_NAME });
 const walletSchema = new mongoose_1.default.Schema({
     privateKey: String,
@@ -35,7 +38,8 @@ const walletSchema = new mongoose_1.default.Schema({
     fundedWalletPublicKey: String,
     mintTransactionHash: String,
     domain: String,
-    created_at: Date
+    created_at: Date,
+    status: String
 });
 const Wallet = mongoose_1.default.model('Wallet', walletSchema);
 const upgradableContractABI = JSON.parse(fs_1.default.readFileSync('./abi/upgradableContract.json', 'utf-8'));
@@ -46,18 +50,71 @@ const TARGET_CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 if (!USDT_CONTRACT_ADDRESS || !TARGET_CONTRACT_ADDRESS) {
     throw new Error("Addresses not provided");
 }
-function mint_domains(fundedWalletPrivateKey, domainsCount, referralCode) {
+// Set up multer for file upload
+const upload = (0, multer_1.default)({ dest: 'uploads/' });
+const serverInstance = axios_1.default.create({
+    baseURL: process.env.SERVER_URL
+});
+function createUser(_a) {
+    return __awaiter(this, arguments, void 0, function* ({ domain, image, hash, walletAddress, referralCode }) {
+        const bodyContent = {
+            "domainAddress": domain,
+            "image": image,
+            "password": walletAddress,
+            "hashCode": hash,
+            "walletAddress": walletAddress,
+            "referralCode": referralCode
+        };
+        const response = yield serverInstance.post("/user/signUpDomain", bodyContent);
+        console.log(response.data);
+    });
+}
+// Function to parse CSV file
+function parseCSV(filePath) {
+    return new Promise((resolve, reject) => {
+        const results = [];
+        fs_1.default.createReadStream(filePath)
+            .pipe((0, csv_parse_1.parse)({ columns: true, skip_empty_lines: true }))
+            .on('data', (data) => results.push(data))
+            .on('end', () => resolve(results))
+            .on('error', (error) => reject(error));
+    });
+}
+function addFamSuffix(str) {
+    if (!str) {
+        return str;
+    }
+    if (!str.endsWith('.fam')) {
+        return str + '.fam';
+    }
+    return str;
+}
+function mint_domains(fundedWalletPrivateKey, referralCode, csvData) {
     return __awaiter(this, void 0, void 0, function* () {
         const wallet = new ethers_1.ethers.Wallet(fundedWalletPrivateKey, provider);
         const usdtContract = new ethers_1.ethers.Contract(USDT_CONTRACT_ADDRESS, usdtABI, wallet);
         const targetContract = new ethers_1.ethers.Contract(TARGET_CONTRACT_ADDRESS, upgradableContractABI, wallet);
-        for (let i = 0; i < domainsCount; i++) {
+        for (let i = 0; i < csvData.length; i++) {
+            const csvRow = csvData[i];
             try {
-                const domain = (0, unique_names_generator_1.uniqueNamesGenerator)(config);
+                const domain = addFamSuffix(csvRow === null || csvRow === void 0 ? void 0 : csvRow.domain);
+                const pfp = csvRow === null || csvRow === void 0 ? void 0 : csvRow.pfp;
+                if (!domain || !pfp) {
+                    return;
+                }
+                console.log("starting minting for domain:", domain);
                 const usdtAmount = ethers_1.ethers.parseUnits('5', 6);
                 const newWallet = ethers_1.ethers.Wallet.createRandom().connect(provider);
-                // const newWallet = new ethers.Wallet(process.env.TARGET_PRIVATE_KEY!, provider);
                 console.log(`New wallet address: ${newWallet.address}`);
+                const walletData = new Wallet({
+                    privateKey: newWallet.privateKey,
+                    publicKey: newWallet.address,
+                    fundedWalletPublicKey: wallet.address.toLowerCase(),
+                    created_at: Date.now(),
+                    domain,
+                    status: "pending"
+                });
+                yield walletData.save();
                 // Get current gas price
                 const feeData = yield provider.getFeeData();
                 const gasPrice = feeData.gasPrice;
@@ -72,7 +129,7 @@ function mint_domains(fundedWalletPrivateKey, domainsCount, referralCode) {
                 // Estimate gas for approve transaction
                 // @ts-ignore
                 const approveGasEstimate = yield usdtContract.connect(newWallet).approve.estimateGas(TARGET_CONTRACT_ADDRESS, usdtAmount);
-                const totalGasBuffer = approveGasEstimate * BigInt(5);
+                const totalGasBuffer = approveGasEstimate * BigInt(BUFFERED_ETH_MULTIPLIER);
                 const requiredEth = totalGasBuffer * gasPrice;
                 console.log({ requiredEth: ethers_1.ethers.formatEther(requiredEth) });
                 const approvalEthTx = yield wallet.sendTransaction({
@@ -89,45 +146,53 @@ function mint_domains(fundedWalletPrivateKey, domainsCount, referralCode) {
                 yield usdtTx.wait();
                 console.log(`Sent 5 USDT to ${newWallet.address}`);
                 // @ts-ignore
-                // const mintGasEstimate = await targetContract.connect(newWallet).mintDomainWithReferral.estimateGas(domain, referralCode);
-                // console.log({ mintGasEstimate })
-                // const requiredEthForMinting = mintGasEstimate * gasPrice;
-                // console.log({ requiredEthForMinting:ethers.formatEther(requiredEthForMinting) })
-                // @ts-ignore
                 const mintTx = yield targetContract.connect(newWallet).mintDomainWithReferral(domain, referralCode);
                 const receipt = yield mintTx.wait();
                 console.log(`Called mintDomainWithReferral for ${domain}`);
                 // Save wallet data to MongoDB
-                const walletData = new Wallet({
-                    privateKey: newWallet.privateKey,
-                    publicKey: newWallet.address,
-                    fundedWalletPublicKey: wallet.address.toLowerCase(),
-                    mintTransactionHash: receipt.hash,
-                    created_at: new Date(),
-                    domain
-                });
+                walletData.mintTransactionHash = receipt.hash,
+                    walletData.status = "success";
                 yield walletData.save();
                 console.log(`Saved wallet data to MongoDB`);
+                yield createUser({
+                    domain,
+                    image: pfp,
+                    hash: receipt.hash,
+                    walletAddress: newWallet.address,
+                    referralCode
+                });
             }
             catch (error) {
-                console.error(`Error in iteration ${i}:`, error);
+                console.error(`Error in iteration ${csvRow}:`, error);
             }
         }
     });
 }
-app.post('/mint', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { private_key, domain_count, referral_code } = req.body;
+app.post('/mint', upload.single('csvFile'), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
+        const csvFile = req.file;
+        const private_key = req.body.private_key;
+        const referral_code = req.body.referral_code;
+        let csvData = [];
+        if (csvFile) {
+            csvData = yield parseCSV(csvFile.path);
+        }
+        console.log({ private_key, referral_code, csvData });
         const wallet = new ethers_1.ethers.Wallet(private_key, provider);
         const usdtContract = new ethers_1.ethers.Contract(USDT_CONTRACT_ADDRESS, usdtABI, wallet);
         const usdtBalance = yield usdtContract.balanceOf(wallet.address);
         const maxMintCount = Math.floor(Number(usdtBalance) / Number(ethers_1.ethers.parseUnits('5', 6)));
-        if (domain_count > maxMintCount) {
-            res.send({ message: "Error:You can mint at max" + maxMintCount + " domains" });
-            return;
-        }
+        if (!Array.isArray(csvData) || csvData.length == 0)
+            if (csvData.length > maxMintCount) {
+                res.send({ message: "Error: You can mint at max " + maxMintCount + " domains" });
+                return;
+            }
         res.json({ message: 'Minting started successfully! Please link below to see mint data live!', publicKey: wallet.address });
-        yield mint_domains(private_key, parseInt(domain_count), referral_code);
+        yield mint_domains(private_key, referral_code, csvData);
+        // Delete the temporary CSV file
+        if (csvFile) {
+            fs_1.default.unlinkSync(csvFile.path);
+        }
     }
     catch (error) {
         console.error('Error during minting:', error);
@@ -151,28 +216,25 @@ app.get('/', (req, res) => {
       </head>
       <body>
           <h1>Domain Minting</h1>
-          <form id="mintForm">
-              <input type="password" id="privateKey" name="privateKey" placeholder="Private Key" required>
-              <input type="text" id="referralCode" name="referralCode" placeholder="Referral Code" required>
-              <input type="number" id="domainCount" name="domainCount" placeholder="Domain Count" required>
+          <form id="mintForm" enctype="multipart/form-data">
+              <input type="password" id="private_key" name="private_key" placeholder="Private Key" required>
+              <input type="text" id="referral_code" name="referral_code" placeholder="Referral Code" required>
+              <input type="file" id="csvFile" name="csvFile" accept=".csv">
               <button type="submit">Start Minting</button>
           </form>
           <div id="loader">Minting in progress... Please wait.</div>
           <div id="result">
-          <a href="/">View minting data</a>
+          <a href="/data">View minting data</a>
           </div>
           <script>
               document.getElementById('mintForm').addEventListener('submit', async (e) => {
                   e.preventDefault();
-                  const privateKey = document.getElementById('privateKey').value;
-                  const domainCount = document.getElementById('domainCount').value;
-                  const referralCode = document.getElementById('referralCode').value;
+                  const formData = new FormData(e.target);
                   document.getElementById('loader').style.display = 'block';
                   try {
                       const response = await fetch('/mint', {
                           method: 'POST',
-                          headers: { 'Content-Type': 'application/json' },
-                          body: JSON.stringify({ private_key: privateKey, domain_count: domainCount, referral_code:referralCode })
+                          body: formData
                       });
                       const result = await response.json();
                       alert(result.message);
@@ -188,7 +250,7 @@ app.get('/', (req, res) => {
     `;
     res.send(html);
 });
-// @ts-ignore
+// @ts-expect-error idk
 app.get("/data", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const entries = yield Wallet.find({});
@@ -212,8 +274,9 @@ app.get("/data", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
           <td>${entry.domain}</td>
           <td title="${(_a = entry.publicKey) === null || _a === void 0 ? void 0 : _a.slice(0, 12)}">${(_b = entry.publicKey) === null || _b === void 0 ? void 0 : _b.slice(0, 12)}</td>
           <td title="${(_c = entry.fundedWalletPublicKey) === null || _c === void 0 ? void 0 : _c.slice(0, 12)}">${(_d = entry.fundedWalletPublicKey) === null || _d === void 0 ? void 0 : _d.slice(0, 12)}</td>
+          <td>${entry.status}</td>
           <td><a href="${process.env.EXPLORER_URL}/tx/${entry.mintTransactionHash}" target="_blank">View Transaction</a></td>
-          <td>${(0, date_fns_1.formatDistanceToNow)(new Date((_e = entry.created_at) === null || _e === void 0 ? void 0 : _e.toDateString()), { includeSeconds: true })}</td>
+          <td>${(0, date_fns_1.format)(new Date((_e = entry.created_at) === null || _e === void 0 ? void 0 : _e.toDateString()), "HH:mm dd MMM yyyy")}</td>
         </tr>
       `;
         }).join('');
@@ -241,11 +304,11 @@ app.get("/data", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
             <table>
                 <thead>
                     <tr>
-
                         <th>No</th>
                         <th>Domain</th>
                         <th>Minted Wallet Public Key</th>
                         <th>Fund Wallet Public Key</th>
+                        <th>Status</th>
                         <th>Mint Transaction</th>
                         <th>Minted at</th>
                     </tr>
@@ -269,6 +332,12 @@ app.get("/data", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         res.status(500).send('<h1>An error occurred while fetching data</h1>');
     }
 }));
-app.listen(port, () => {
+app.listen(port, () => __awaiter(void 0, void 0, void 0, function* () {
     console.log(`Server running at http://localhost:${port}`);
-});
+    const files = yield fs_1.default.readdirSync("uploads");
+    files.forEach(file => {
+        if (!file.includes("gitkeep")) {
+            fs_1.default.unlinkSync(`uploads/${file}`);
+        }
+    });
+}));
